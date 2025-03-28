94314d5c974f55afa51071e632a8b0dd0acd31e5
Luca Rubin <luca.rubin@intesys.it>
2025-03-28 16:30:16 +0100

    App.java: sample code

--- src/main/java/com/lucarubin/App.java
+++ src/main/java/com/lucarubin/App.java
@@ -3,11 +3,19 @@ package com.lucarubin;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.lucarubin.models.SingleZoneForecast;
+import com.lucarubin.services.ForecastService;
+import com.lucarubin.services.ForecastServiceImpl;
+import com.lucarubin.services.ZoneService;
+import com.lucarubin.services.ZoneServiceImpl;
+import it.redturtle.mobile.apparpav.types.Meteogram;
+import it.redturtle.mobile.apparpav.types.Municipality;
+import it.redturtle.mobile.apparpav.utils.Global;
 import it.redturtle.mobile.apparpav.utils.Util;
 import it.redturtle.mobile.apparpav.utils.XMLParser;
 
 import java.io.IOException;
-import java.util.Optional;
+import java.util.List;
 
 /**
  * Hello world!
@@ -18,19 +26,35 @@ public class App
     public static void main( String[] args )
     {
 
-        // input
-        int zoneId = 11; // Vicenza
+        testConvertJson();
 
-        String xml = Util.getXML(null);
-        Optional<MeteoData> meteoData = XMLParser.getXML(xml);
+        ZoneService zoneService = new ZoneServiceImpl();
+        List<Municipality> comuniPadova = zoneService.getComuniByProvincia("Padova");
 
+        System.out.println(comuniPadova.stream().findFirst().map(Municipality::getZoneid).orElse("nd"));
 
-        testConvertJson(xml);
+        XMLParser.getXML(null, Util.getXML(null));
+        Meteogram meteograms = Global.istance().getMeteogramsByZoneIDS(comuniPadova.stream().findFirst().map(Municipality::getZoneid).orElse("nd"));
 
+        ForecastService forecastService = new ForecastServiceImpl(new ZoneServiceImpl());
+        List<SingleZoneForecast> allForecasts = forecastService.getAllForecasts();
+
+        // Converti la lista allForecasts in JSON e stampala
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            // Converte la lista allForecasts in una stringa JSON formattata
+            String allForecastsJson = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(allForecasts);
+            System.out.println(allForecastsJson);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
-    private static void testConvertJson(String xml) {
+    private static void testConvertJson() {
+
+        String xml = Util.getXML(null);
 
         try {
             // Creare un ObjectMapper XML
